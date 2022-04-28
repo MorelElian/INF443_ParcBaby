ef1e0b0a6d5f14218a15f89ce336e0a89c2b23ee
navaxel <104438099+navaxel@users.noreply.github.com>
2022-04-28 18:21:13 +0200

    Add files via upload

---
+++ parcbaby/src/elementar.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "cgp/cgp.hpp"
+
+
+cgp::mesh create_torus_mesh(float radius, float thickness, float start_angle, float end_angle, int radial_resolution, int tubular_resolution);
+cgp::mesh mesh_primitive_parapede(float longueur, float largeur, float hauteur);
+cgp::mesh mesh_primitive_smoothCylinder(float radius, float height);
---
+++ parcbaby/src/racetrack.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "cgp/cgp.hpp"
+
+
+cgp::hierarchy_mesh_drawable create_racetrack_mesh_drawable();
